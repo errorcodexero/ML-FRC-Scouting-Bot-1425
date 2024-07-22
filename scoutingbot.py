ebe3f65a40092295767d9dbcf10366ab590e0586
Michael <107226999+themehdev@users.noreply.github.com>
2024-07-21 20:48:09 -0700

    did it i think
    it says we not that good tho

--- scoutingbot.py
+++ scoutingbot.py
@@ -1,185 +1,18 @@
+import math
 import statbotics
 
 sb = statbotics.Statbotics()
 
-LEARNING_RATE = 1000000000
-H = 0.00001
-print(H)
+LEARNING_RATE0 = 0.000000008
+LEARNING_RATE1 = 0.0000000000025
+LEARNING_RATE2 = 0.000000000000005
+H = 0.000000000000001
 
-a_this = 1
-a_opp = 1
-b_this = 1
-b_opp = 1
-c_this = 1
-c_opp = 1
-d = 1
+IS_EPA = False
+INDIVIDUAL_DONE_THRESH = 3
+DONE_THRESH = 0.000005
 
 year = int(input("enter year:\n"))
-
-print("Getting some test matches...")
-year_matches = sb.get_matches(None, year)
-#year_matches = year_matches[:1000]
-
-team_data = []
-team_training_vals = []
-
-print("Inintalizing data set...")
-for i in range(10000):
-    team_data.append({"this": [], "opp": []})
-
-def get_pred_next_score(scores):
-    a = 1
-    b = 1
-    c = 1
-
-    da = 100
-    db = 100
-    dc = 100
-
-    def quadratic(a, b, c, x):
-        return a * x * x + b * x + c
-    
-    def cost(scores, a, b, c):
-        ret = 0
-        for i in range(len(scores) - 1):
-            if(scores[i] == None):
-                continue
-            ret += abs(scores[i] - quadratic(a, b, c, i)) ** 2
-        ret /= len(scores)
-        return ret
-    
-    while(abs(da) > 0.001 or abs(db) > 0.001 or abs(dc) > 0.001):
-        da = cost(scores, a + H, b, c) - cost(scores, a, b, c)
-        #print(cost(scores, a + H, b, c), cost(scores, a, b, c))
-        da /= H
-        db = cost(scores, a, b + H, c) - cost(scores, a, b, c)
-        db /= H
-        dc = cost(scores, a, b, c + H) - cost(scores, a, b, c)
-        dc /= H
-        a -= LEARNING_RATE * da
-        b -= LEARNING_RATE * db
-        c -= LEARNING_RATE * dc
-
-        avg_err = abs(da) + abs(db) + abs(dc)
-        avg_err /= 3
-        print(f"Fitting a curve, model has {cost(scores, a, b, c)} , {avg_err} error", da, ":", db, ":", dc, "   ", a, ":", b, ":", c)
-    
-    return quadratic(a, b, c, len(scores))
-
-def cubic(a, b, c, d, x):
-    return a * x * x * x + b * x * x + c * x + d
-
-def cubic_for_ret(x1_this, x2_this, x3_this, x1_opp, x2_opp, x3_opp, which = None):
-    ret = 0
-
-    a_ttmp = a_this + H if which == "at" else a_this
-    b_ttmp = b_this + H if which == "bt" else b_this
-    c_ttmp = c_this + H if which == "ct" else c_this
-    a_otmp = a_opp + H if which == "ao" else a_opp
-    b_otmp = b_opp + H if which == "bo" else b_opp
-    c_otmp = c_opp + H if which == "co" else c_opp
-    d_tmp = d + d if which == "d" else d
-
-    ret += cubic(a_ttmp, b_ttmp, c_ttmp, d_tmp/6, x1_this)
-    ret += cubic(a_ttmp, b_ttmp, c_ttmp, d_tmp/6, x2_this)
-    ret += cubic(a_ttmp, b_ttmp, c_ttmp, d_tmp/6, x3_this)
-    ret += cubic(a_otmp, b_otmp, c_otmp, d_tmp/6, x1_opp)
-    ret += cubic(a_otmp, b_otmp, c_otmp, d_tmp/6, x2_opp)
-    ret += cubic(a_otmp, b_otmp, c_otmp, d_tmp/6, x3_opp)
-    return ret
-
-
-def train_model(year_matches):
-    counter = 0
-    for match in year_matches:
-        counter += 1
-        print(f"Getting match {counter} data...")
-        team_data[match["blue_1"]]["this"].append(match["blue_score"])
-        team_data[match["blue_1"]]["opp"].append(match["red_score"])
-        team_data[match["blue_2"]]["this"].append(match["blue_score"])
-        team_data[match["blue_2"]]["opp"].append(match["red_score"])
-        team_data[match["blue_3"]]["this"].append(match["blue_score"])
-        team_data[match["blue_3"]]["opp"].append(match["red_score"])
-
-        team_data[match["red_1"]]["this"].append(match["red_score"])
-        team_data[match["red_1"]]["opp"].append(match["blue_score"])
-        team_data[match["red_2"]]["this"].append(match["red_score"])
-        team_data[match["red_2"]]["opp"].append(match["blue_score"])
-        team_data[match["red_3"]]["this"].append(match["red_score"])
-        team_data[match["red_3"]]["opp"].append(match["blue_score"])
-
-        team_training_vals.append({"score" : match["blue_score"],
-                                   "t1pred" : get_pred_next_score(team_data[match["blue_1"]]["this"]), 
-                                   "t2pred" : get_pred_next_score(team_data[match["blue_2"]]["this"]), 
-                                   "t3pred" : get_pred_next_score(team_data[match["blue_3"]]["this"]), 
-                                   "t1opp_pred" : get_pred_next_score(team_data[match["red_1"]]["opp"]), 
-                                   "t2opp_pred" : get_pred_next_score(team_data[match["red_2"]]["opp"]), 
-                                   "t3opp_pred" : get_pred_next_score(team_data[match["red_3"]]["opp"])})
-        
-        team_training_vals.append({"score" : match["red_score"],
-                                   "t1pred" : get_pred_next_score(team_data[match["red_1"]]["this"]), 
-                                   "t2pred" : get_pred_next_score(team_data[match["red_2"]]["this"]), 
-                                   "t3pred" : get_pred_next_score(team_data[match["red_3"]]["this"]), 
-                                   "t1opp_pred" : get_pred_next_score(team_data[match["blue_1"]]["opp"]), 
-                                   "t2opp_pred" : get_pred_next_score(team_data[match["blue_2"]]["opp"]), 
-                                   "t3opp_pred" : get_pred_next_score(team_data[match["blue_3"]]["opp"])})
-        
-    
-    a_this = 1
-    a_opp = 1
-    b_this = 1
-    b_opp = 1
-    c_this = 1
-    c_opp = 1
-    d = 1
-
-    da_this = 1
-    da_opp = 1
-    db_this = 1
-    db_opp = 1
-    dc_this = 1
-    dc_opp = 1
-    dd = 1
-    
-    def cost(which = None):
-        ret = 0
-        for val in team_training_vals:
-            if(val["score"] == None):
-                continue
-            ret += abs(val["score"] - cubic_for_ret(val["t1pred"], val["t2pred"], val["t3pred"], val["t1opp_pred"], val["t2opp_pred"], val["t3opp_pred"], which)) ** 2
-        ret /= len(team_training_vals)
-        return ret
-    
-    while abs(da_opp) > 0.01 or abs(da_this) > 0.01 or abs(db_opp > 0.01) or abs(db_this > 0.01) or abs(dc_opp > 0.01) or abs(dc_this) > 0.01 or abs(dd) > 0.01 :
-        avg_error = abs(da_this) + abs(da_opp) + abs(db_this) + abs(db_opp) + abs(dc_this) + abs(dc_opp) + abs(dd)
-        avg_error /= 7
-        print(f"Training model, Lin reg has {avg_error} error")
-        da_this = cost("at") - cost()
-        da_this /= H
-        da_opp = cost("ao") - cost()
-        da_opp /= H
-        db_this = cost("bt") - cost()
-        db_this /= H
-        db_opp = cost("bo") - cost()
-        db_opp /= H
-        dc_this = cost("ct") - cost()
-        dc_this /= H
-        dc_opp = cost("co") - cost()
-        dc_opp /= H
-        dd = cost("d") - cost()
-        dd /= H
-
-        a_this -= LEARNING_RATE * da_this
-        a_opp -= LEARNING_RATE * da_opp
-        b_this -= LEARNING_RATE * db_this
-        b_opp -= LEARNING_RATE * db_opp
-        c_this -= LEARNING_RATE * dc_this
-        c_opp -= LEARNING_RATE * dc_opp
-        d -= LEARNING_RATE * dd
-
-    avg_error = abs(da_this) + abs(da_opp) + abs(db_this) + abs(db_opp) + abs(dc_this) + abs(dc_opp) + abs(dd)
-    avg_error /= 7
-    print(f"Finished training with {avg_error} error")
 
 t1num = int(input("enter team 1:\n"))
 t2num = int(input("enter team 2:\n"))
@@ -233,30 +66,232 @@ oppT1match_scores = get_match_scores(oppT1num)
 oppT2match_scores = get_match_scores(oppT2num)
 oppT3match_scores = get_match_scores(oppT3num)
 
-print("Getting team match predictions...")
-t1next_pred = get_pred_next_score(t1match_scores)
-t2next_pred = get_pred_next_score(t2match_scores)
-t3next_pred = get_pred_next_score(t3match_scores)
+def get_test_matches(team):
+    if(team == 0):
+        return []
+    print(f"Getting some test matches for team {team}...")
+    team_last_event = sb.get_team_events(team, year)[-1]
+    return sb.get_matches(None, None, team_last_event["event"])
 
-t1opp_next_pred = get_pred_next_score(t1opp_match_scores)
-t2opp_next_pred = get_pred_next_score(t2opp_match_scores)
-t3opp_next_pred = get_pred_next_score(t3opp_match_scores)
+def abs_avg(dict):
+    ret = 0
+    for i in dict:
+        if(i != None):
+            ret += abs(i)
+    ret /= len(dict)
+    return ret
 
-print("Getting opposing team match predictions...")
-oppT1opp_next_pred = get_pred_next_score(oppT1opp_match_scores)
-oppT2opp_next_pred = get_pred_next_score(oppT2opp_match_scores)
-oppT3opp_next_pred = get_pred_next_score(oppT3opp_match_scores)
+training_matches = []
+training_matches += get_test_matches(t1num)
+training_matches += get_test_matches(t2num)
+training_matches += get_test_matches(t3num)
+training_matches += get_test_matches(oppT1num)
+training_matches += get_test_matches(oppT2num)
+training_matches += get_test_matches(oppT3num)
 
-oppT1next_pred = get_pred_next_score(oppT1match_scores)
-oppT2next_pred = get_pred_next_score(oppT2match_scores)
-oppT3next_pred = get_pred_next_score(oppT3match_scores)
+print("Initializing training set...")
+
+team_match_scores = []
+opp_team_match_scores = []
+
+training_set = []
+
+for i in range(10000):
+    team_match_scores.append([])
+    opp_team_match_scores.append([])
+
+for match in training_matches :
+    if(len(team_match_scores[match["blue_1"]]) > 0 and
+       len(team_match_scores[match["blue_2"]]) > 0 and
+       len(team_match_scores[match["blue_3"]]) > 0 and
+       len(team_match_scores[match["red_1"]]) > 0 and
+       len(team_match_scores[match["red_2"]]) > 0 and
+       len(team_match_scores[match["red_3"]]) > 0 and
+       match["blue_score"] != None):
+        training_set.append({"inputs": [abs_avg(team_match_scores[match["blue_1"]]), 
+                            team_match_scores[match["blue_1"]][-1],
+                            abs_avg(team_match_scores[match["blue_2"]]),
+                            team_match_scores[match["blue_2"]][-1],
+                            abs_avg(team_match_scores[match["blue_3"]]), 
+                            team_match_scores[match["blue_3"]][-1],
+                            abs_avg(opp_team_match_scores[match["red_1"]]),
+                            opp_team_match_scores[match["red_1"]][-1],
+                            abs_avg(opp_team_match_scores[match["red_2"]]),
+                            opp_team_match_scores[match["red_2"]][-1],
+                            abs_avg(opp_team_match_scores[match["red_3"]]),
+                            opp_team_match_scores[match["red_3"]][-1]],
+                            "output": match["blue_score"]})
+    if(len(team_match_scores[match["blue_1"]]) > 0 and
+       len(team_match_scores[match["blue_2"]]) > 0 and
+       len(team_match_scores[match["blue_3"]]) > 0 and
+       len(team_match_scores[match["red_1"]]) > 0 and
+       len(team_match_scores[match["red_2"]]) > 0 and
+       len(team_match_scores[match["red_3"]]) > 0 and
+       match["blue_score"] != None):
+        training_set.append({"inputs": [abs_avg(team_match_scores[match["red_1"]]), 
+                            team_match_scores[match["red_1"]][-1],
+                            abs_avg(team_match_scores[match["red_2"]]),
+                            team_match_scores[match["red_2"]][-1],
+                            abs_avg(team_match_scores[match["red_3"]]), 
+                            team_match_scores[match["red_3"]][-1],
+                            abs_avg(opp_team_match_scores[match["blue_1"]]),
+                            opp_team_match_scores[match["blue_1"]][-1],
+                            abs_avg(opp_team_match_scores[match["blue_2"]]),
+                            opp_team_match_scores[match["blue_2"]][-1],
+                            abs_avg(opp_team_match_scores[match["blue_3"]]),
+                            opp_team_match_scores[match["blue_3"]][-1]],
+                            "output": match["red_score"]})
+    if(match["blue_score"] != None):
+        team_match_scores[match["blue_1"]].append(match["blue_score"])
+        team_match_scores[match["blue_2"]].append(match["blue_score"])
+        team_match_scores[match["blue_3"]].append(match["blue_score"])
+
+        opp_team_match_scores[match["red_1"]].append(match["blue_score"])
+        opp_team_match_scores[match["red_2"]].append(match["blue_score"])
+        opp_team_match_scores[match["red_3"]].append(match["blue_score"])
+    
+    if(match["red_score"] != None):
+        team_match_scores[match["red_1"]].append(match["red_score"])
+        team_match_scores[match["red_2"]].append(match["red_score"])
+        team_match_scores[match["red_3"]].append(match["red_score"])
+
+        opp_team_match_scores[match["blue_1"]].append(match["red_score"])
+        opp_team_match_scores[match["blue_2"]].append(match["red_score"])
+        opp_team_match_scores[match["blue_3"]].append(match["red_score"])
 
 print("Starting training...")
-train_model(year_matches)
+
+a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+c = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+d = 0
+
+def cubic_thing(x, which = ""):
+    ret = 0
+    for i in range(len(x)):
+        if(which == f"a{i}"):
+            ret += (a[i] + H) * x[i] ** 3
+            ret += b[i] * x[i] ** 2
+            ret += c[i] * x[i]
+        elif(which == f"b{i}"):
+            ret += a[i] * x[i] ** 3
+            ret += (b[i] + H) * x[i] ** 2
+            ret += c[i] * x[i]
+        elif(which == f"c{i}"):
+            ret += a[i] * x[i] ** 3
+            ret += b[i] * x[i] ** 2
+            ret += (c[i] + H) * x[i]
+        else:
+            ret += a[i] * x[i] ** 3
+            ret += b[i] * x[i] ** 2
+            ret += c[i] * x[i]
+    
+    if(which == "d"):
+        ret += d + H
+    else:
+        ret += d
+    
+    return ret
+
+def cost(which = ""):
+    ret = 0
+    for datum in training_set:
+        ret += abs(cubic_thing(datum["inputs"], which) - datum["output"])
+    ret /= len(training_set)
+    return ret
+
+da = []
+db = []
+dc = []
+dd = INDIVIDUAL_DONE_THRESH + 1
+
+for i in range(12):
+    da.append(INDIVIDUAL_DONE_THRESH + 1)
+    db.append(INDIVIDUAL_DONE_THRESH + 1)
+    dc.append(INDIVIDUAL_DONE_THRESH + 1)
+
+is_done = []
+for i in range(37):
+    is_done.append(False)
+
+def percent_done():
+    ret = 0
+    for i in is_done:
+        if i == True:
+            ret += 1
+    ret /= len(is_done)
+    ret *= 100
+    return ret
+
+last_cost = 0
+counter = 0
+while True:
+    counter += 1
+    last_cost = cost()
+    for i in range(len(da)):
+        if(abs(da[i]) < INDIVIDUAL_DONE_THRESH):
+            is_done[i] = True
+        else: 
+            da[i] = (cost(f"a{i}") - cost()) / H
+        
+        if(abs(db[i]) < INDIVIDUAL_DONE_THRESH):
+            is_done[i + 12] = True
+        else:
+            db[i] = (cost(f"b{i}") - cost()) / H
+
+        if(abs(dc[i]) < INDIVIDUAL_DONE_THRESH):
+            is_done[i + 24] = True
+        else:         
+            dc[i] = (cost(f"c{i}") - cost()) / H
+                  
+    if(abs(dd) < INDIVIDUAL_DONE_THRESH or IS_EPA):
+        is_done[36] = True
+    else:
+        dd = (cost("dd") - cost()) / H
+
+    for i in range(len(a)):
+        if(not is_done[i]):
+            a[i] -= (da[i] ** 2) * math.copysign(1, da[i]) * LEARNING_RATE2 + da[i] * LEARNING_RATE1 + math.copysign(1, da[i]) * LEARNING_RATE0
+        if(not is_done[i + 12]):
+            b[i] -= (db[i] ** 2) * math.copysign(1, db[i]) * LEARNING_RATE2 + db[i] * LEARNING_RATE1 + math.copysign(1, db[i]) * LEARNING_RATE0
+        if(not is_done[i + 24]):
+            c[i] -= (dc[i] ** 2) * math.copysign(1, dc[i]) * LEARNING_RATE2 + dc[i] * LEARNING_RATE1 + math.copysign(1, dc[i]) * LEARNING_RATE0
+    if(not is_done[36]):
+        d -= (dd ** 2) * math.copysign(1, dd) * LEARNING_RATE2 + dd * LEARNING_RATE1 + math.copysign(1, dd) * LEARNING_RATE0
+
+    if(last_cost - cost() < DONE_THRESH):
+        break
+
+    if(counter % 5 == 0):
+        print(f"Training model, model has {round(abs_avg([abs_avg(da), abs_avg(db), abs_avg(dc), dd])* 1000)/1000} error, {round(cost() * 1000)/1000} variance, {round(percent_done() * 100)/100}% of values done calcualting")
+
 print("Model done training!")
+print(f"Model finished with {cost()} variance.")
 
-this_pred = cubic_for_ret(t1next_pred, t2next_pred, t3next_pred, oppT1opp_next_pred, oppT2opp_next_pred, oppT3opp_next_pred)
-opp_pred = cubic_for_ret(oppT1next_pred, oppT2next_pred, oppT3next_pred, t1opp_next_pred, t2opp_next_pred, t3opp_next_pred)
 
-print(f"This team predicted score: {this_pred}")
-print(f"Opponent team predicted score: {opp_pred}")
+print(f"This team predicted score: {cubic_thing([abs_avg(t1match_scores),
+                                                 t1match_scores[-1],
+                                                 abs_avg(t2match_scores),
+                                                 t2match_scores[-1],
+                                                 abs_avg(t3match_scores),
+                                                 t3match_scores[-1],
+                                                 abs_avg(oppT1opp_match_scores),
+                                                 oppT1opp_match_scores[-1],
+                                                 abs_avg(oppT2opp_match_scores),
+                                                 oppT2opp_match_scores[-1],
+                                                 abs_avg(oppT3opp_match_scores),
+                                                 oppT3opp_match_scores[-1]
+                                                 ])}")
+print(f"Opponent team predicted score: {cubic_thing([abs_avg(oppT1match_scores),
+                                                 oppT1match_scores[-1],
+                                                 abs_avg(oppT2match_scores),
+                                                 oppT2match_scores[-1],
+                                                 abs_avg(oppT3match_scores),
+                                                 oppT3match_scores[-1],
+                                                 abs_avg(t1opp_match_scores),
+                                                 t1opp_match_scores[-1],
+                                                 abs_avg(t2opp_match_scores),
+                                                 t2opp_match_scores[-1],
+                                                 abs_avg(t3opp_match_scores),
+                                                 t3opp_match_scores[-1]
+                                                 ])}")
